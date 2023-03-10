df828e3809cc92b4782f7b61e33783d7ae82b5da
rkham93 <rohith.kbmettu@gmail.com>
2023-03-10 12:57:34 +0530

    added loader functions and refactored code

--- madlibs/madlibs_generator.py
+++ madlibs/madlibs_generator.py
@@ -1,39 +1,69 @@
 
 import time
-def delay(delay_time):
-    time.sleep(delay_time)
+innerBrace='<'
+outerBrace='>'
+Story='I was on my way to the <place> yesterday, where I went to pick up <noun>, When I noticed that all the <plural object> near the <place> were <color>. It was <name of season> indeed.'
+def loader():
+    pass
+
+def clearLine(line):
+    print ("\033[A"+ ' '*len(line) +"\033[A")
+
+def addWhiteSpaces(String):
+    return ' '+String+' '
+
+def delay(delay_time=1,bufferSymbol = '<->'):
+    startTime=time.time()
+    while time.time() < startTime+delay_time:
+        for i in range(5):
+            line = addWhiteSpaces(bufferSymbol) *i
+            print(line)
+            time.sleep(0.25)            
+            clearLine(line)
+        i=0
 
 def newLine(n):
     print('\n'*n)         
     
-
 def Print(String):    
     for i in range(len(String)+1):
         toPrint=String[0:i]
-        print ("\033[A"+ ' '*len(toPrint) +"\033[A")
+        clearLine(toPrint)
         print(toPrint)
         time.sleep(0.025)
+
+def getBlanks(Story):
+    chevronList=[]
+    angleBraceletList=[]
+    for i in range(len(Story)):
+        
+        if Story[i] == innerBrace:
+            chevronList.append(i)
+        elif Story[i]== outerBrace:
+            angleBraceletList.append(i)
+
+    Blanks=[]
+    for i in range(len(chevronList)):
+        Blanks.append(Story[chevronList[i]+1:angleBraceletList[i]])
+    
+    return Blanks
+
         
 def MadLibs(Story):
-    Blanks=['place','noun','plural object','place','color','name of season']
+    Blanks=getBlanks(Story)
     userInputs=[]
     for Blank in Blanks:
         userInput=input('Enter a {}: '.format(Blank))
         userInputs.append(userInput)
-        Story=Story.replace('<'+Blank+'>','"'+userInput+'"',1)
+        Story=Story.replace(innerBrace+Blank+outerBrace,'"'+userInput.capitalize()+'"',1)
     
     print('Thanks for the inputs ...') 
+    delay(2)
+    print('Generating your cool MadLibs !!!')
     newLine(1)
-    delay(1)
-    print('Please wait while we get your results ...')
-    newLine(1)
-    delay(1)
-    print('The results are ready ...')
-    newLine(1)
-    delay(1)
-    newLine(1)
+    delay(5)
+    
     return Story
 
-Story='I was on my way to the <place> yesterday, where I went to pick up <noun>, When I noticed that all the <plural object> near the <place> were <color>. It was <name of season> indeed.'
 mablibs_generated=MadLibs(Story)
 Print(mablibs_generated)
