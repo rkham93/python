34460071813695e59853e5d254a875aafeabffb0
rkham93 <rohith.kbmettu@gmail.com>
2023-03-10 16:08:02 +0530

    Refactored Code

---
+++ madlibs/app.py
@@ -0,0 +1,56 @@
+
+import json
+
+from utils import Print, delay, newLine
+
+
+def setTheme():
+    file=open("madlibs\Themes.json")
+    Themes=json.load(file)
+    ThemesList=list(Themes.keys())
+    InputStatement='Please select a theme from the following:\n'
+    for i in range(len(ThemesList)):
+        InputStatement+=" {i}: {theme}\n".format(i=i+1,theme=ThemesList[i])
+    Theme=input(InputStatement)
+    Print('you have chosen the "{Theme}" Theme!'.format(Theme=ThemesList[int(Theme)-1]))
+    newLine()
+    return Themes[ThemesList[int(Theme)-1]]
+
+
+def getBlanks(Story):
+    innerBraceList=[]
+    outerBraceletList=[]
+    for i in range(len(Story)):
+        
+        if Story[i] == innerBrace:
+            innerBraceList.append(i)
+        elif Story[i]== outerBrace:
+            outerBraceletList.append(i)
+    Blanks=[]
+    for i in range(len(innerBraceList)):
+        Blanks.append(Story[innerBraceList[i]+1:outerBraceletList[i]])
+    
+    return Blanks
+
+def MadLibs(Story):
+    Blanks=getBlanks(Story)
+    for Blank in Blanks:
+        Line='Enter a {}: \n'.format(Blank)
+        userInput=input(Line)     
+        Story=Story.replace(innerBrace+Blank+outerBrace,'"'+userInput.capitalize()+'"',1)
+    newLine()
+    Print('Thanks for the inputs ...') 
+    delay('small')
+    newLine()
+    Print('Generating your cool MadLibs !!!')    
+    newLine()
+    delay('large')
+    Print(Story)
+
+if __name__ == "__main__":
+    theme=setTheme()
+    innerBrace=theme["innerBrace"]
+    outerBrace=theme["outerBrace"]
+    Story=theme["Story"]
+    MadLibs(Story)
+ 
--- madlibs/readme.txt
+++ madlibs/readme.txt
@@ -1,2 +1,11 @@
-This project is a mad libs generator. 
+This project is a Mad Libs generator. 
 The first version is to create a user inputs one by one and print out the generated madlibs.
+
+Date: Mar 10 2023
+Refactored code: 
+    + All the helper functions are now in utils.py 
+    + The app functions are in app.py
+    + The app now has 2 themes to choose from
+    + The loader function is added to display an icon during waiting times
+    + run app.py to run the app in your terminal
+
