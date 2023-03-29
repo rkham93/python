06e797d7e44d7ff1d1b801414ee6c4e9eed6c546
rkham93 <rohith.kbmettu@gmail.com>
2023-03-29 23:04:32 +0530

    refactored code

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ from GuessTheNumber import number_guesser
 from HangMan import hangman
 
 gameList={"Guess the number":number_guesser,"Hang Man":hangman,"Mad Libs":madlibs}
+
 if __name__ =="__main__":
-    userInput=int(input('Which game do you want to play?\n'+"\n".join(list(gameList.keys()))+'\n'))
+    UserInfo={}
+    Name=input("Enter your name?\n")
+    gameNames=list(gameList.keys())
+    userInput=int(input('Which game do you want to play?\n'+"\n".join(gameNames))+'\n')
+    Game=gameNames[userInput]
+    UserInfo['username']=Name
+    UserInfo['game played']=Game
     userInput-=1
-    gameList[list(gameList.keys())[userInput]].main()
+    gameList[gameNames[userInput]].main()
